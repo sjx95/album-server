d4a0e4c1d8876247bb6556a24a3df8df516c6cc5
FukoMaster <sjx95@outlook.com>
2019-04-12 23:29:47 +0800

    Add conflict check.

--- album-server/Pages/Upload.cshtml.cs
+++ album-server/Pages/Upload.cshtml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http.Internal;
@@ -19,6 +20,9 @@ namespace album_server.Pages
         [BindProperty] public FormFileCollection Pictures { get; set; }
         [BindProperty] public bool AppendMode { get; set; }
 
+        public HashSet<string> FileSet = new HashSet<string>();
+        public HashSet<string> ConflictSet = new HashSet<string>();
+
         public IActionResult OnPost()
         {
             if (!ModelState.IsValid)
@@ -26,19 +30,73 @@ namespace album_server.Pages
                 return BadRequest();
             }
 
-            foreach (var pic in Pictures)
+            if (AppendMode && System.IO.File.Exists($"{hostEnv.WebRootPath}/userdata/{DeviceId}.txt"))
             {
-                var path = $"{hostEnv.WebRootPath}/userdata/{DeviceId}/{pic.FileName}";
-                FileInfo fileInfo = new FileInfo(path);
-                var di = fileInfo.Directory;
-                if (!di.Exists)
-                    di.Create();
-                FileStream file = System.IO.File.OpenWrite(path);
-                pic.CopyTo(file);
-                file.Close();
+                foreach (var fn in System.IO.File.ReadLines($"{hostEnv.WebRootPath}/userdata/{DeviceId}.txt"))
+                {
+                    FileSet.Add(fn);
+                }
             }
 
+            foreach (var pic in Pictures)
+            {
+                if (FileSet.Contains(pic.FileName))
+                {
+                    var oldFile = System.IO.File.OpenRead($"{hostEnv.WebRootPath}/userdata/{DeviceId}/{pic.FileName}");
+
+                    bool conflict = false;
+                    if (pic.Length == oldFile.Length)
+                    {
+                        var newFile = pic.OpenReadStream();
+                        for (long i = 0; i < pic.Length; ++i)
+                        {
+                            if (newFile.ReadByte() != oldFile.ReadByte())
+                            {
+                                conflict = true;
+                                break;
+                            }
+                        }
+                        newFile.Close();
+                    }
+                    else
+                    {
+                        conflict = true;
+                    }
+
+                    oldFile.Close();
+
+                    if (conflict)
+                    {
+                        ConflictSet.Add(pic.FileName);
+                    }
+
+                    continue;
+                }
+
+                FileStream picFs = CreateAndOpenWrite($"{hostEnv.WebRootPath}/userdata/{DeviceId}/{pic.FileName}");
+                pic.CopyTo(picFs);
+                picFs.Close();
+
+                FileSet.Add(pic.FileName);
+            }
+
+            var fsw = new StreamWriter(CreateAndOpenWrite($"{hostEnv.WebRootPath}/userdata/{DeviceId}.txt"));
+            foreach (var fn in FileSet)
+            {
+                fsw.WriteLine(fn);
+            }
+            fsw.Close();
+
             return Page();
+        }
+
+        private FileStream CreateAndOpenWrite(string path)
+        {
+            FileInfo fileInfo = new FileInfo(path);
+            var di = fileInfo.Directory;
+            if (!di.Exists)
+                di.Create();
+            return System.IO.File.OpenWrite(path);
         }
 
         private readonly IHostingEnvironment hostEnv;
