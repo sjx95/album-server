9194cd0a130540e019665c0e33f877dedc1a6f88
FukoMaster <sjx95@outlook.com>
2019-04-12 14:18:31 +0800

    Upload page nearly OK.

---
+++ album-server/Pages/Upload.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http.Internal;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace album_server.Pages
+{
+    [IgnoreAntiforgeryToken(Order = 1010)]
+    [RequestSizeLimit(200 * 1024 * 1024)]
+    public class UploadModel : PageModel
+    {
+        [BindProperty(SupportsGet = true)] public UInt64 DeviceId { get; set; } 
+        [BindProperty] public FormFileCollection Pictures { get; set; }
+    }
+}
